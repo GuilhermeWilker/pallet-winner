58ad21efc1068f870002f223f184a506e7d6c737
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-03-07 19:11:58 -0300

    initial commit: Hello World!

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+  <!-- <header>
+    <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+
+  <Navbar />
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul class="nav justify-content-between">
+      <ul class="nav justify-content-between">
+        <li class="nav-item">
+          <RouterLink to="/" class="nav-link active">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/about" class="nav-link">About</RouterLink>
+        </li>
+      </ul>
+
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">LOGO</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About us</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<style scoped></style>
